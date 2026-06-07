16be6cce4fce01e30a5697acab6b28ce80c22ef3
abrahameishow <abrahameishow95@gmail.com>
2026-06-07 20:48:14 +0200

    Initial Commit

---
+++ assyrian-bible-mobile-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  assyrian-bible-mobile-IOS
+//
+//  Created by Abraham Eishow on 2026-06-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ assyrian-bible-mobile-IOS/assyrian_bible_mobile_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  assyrian_bible_mobile_IOSApp.swift
+//  assyrian-bible-mobile-IOS
+//
+//  Created by Abraham Eishow on 2026-06-07.
+//
+
+import SwiftUI
+
+@main
+struct assyrian_bible_mobile_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
